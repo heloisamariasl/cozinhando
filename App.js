ffe5bcc4b66e08ec3155bac86228354dbbd4e7a0
heloisamariasl <heloisamsleite@gmail.com>
2025-04-23 14:35:40 -0300

    tela1

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function CozinhandoComChico() {
+ return (
+  <View style={styles.container}>
+   <Image
+    source={require('./assets/chico.png')} // coloque sua imagem aqui
+    style={styles.image}
+    resizeMode="contain"
+   />
+   <Text style={styles.text}>Cozinhando com o Chico</Text>
+  </View>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#F5DDC7' // cor bege do fundo
+ },
+ image: {
+  width: 200,
+  height: 200,
+  marginBottom: 20
+ },
+ text: {
+  fontSize: 20,
+  color: '#7CA8A1',
+  fontWeight: 'bold'
+ }
 });
+
